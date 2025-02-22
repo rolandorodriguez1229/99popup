b97b882e7f676af111b5537fa31b8fe5eb5f2183
rolandorodriguez1229 <61759440+rolandorodriguez1229@users.noreply.github.com>
2025-02-22 13:30:59 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Optimizaciones para producción
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
 }
 
 module.exports = nextConfig
