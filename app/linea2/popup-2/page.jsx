9d493d36d8c5a4a21c258d3e8aa1f91e7b639202
rolandorodriguez1229 <61759440+rolandorodriguez1229@users.noreply.github.com>
2025-02-26 11:41:33 -0600

    Update page.jsx

--- app/linea2/popup-2/page.jsx
+++ app/linea2/popup-2/page.jsx
@@ -34,9 +34,9 @@ export default function Station99Page() {
   
   return (
     <StationView 
-      stationName="mesa"    // Este valor será "99", "popup", "ventanas" o "mesa"
+      stationName="popup"    // Este valor será "99", "popup", "ventanas" o "mesa"
       lineNumber={2}      // 1 o 2 dependiendo de la línea 
-      title="Mesa #2"        // Título que se mostrará en la página
+      title="PopUp #2"        // Título que se mostrará en la página
     />
   );
 }
